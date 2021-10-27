21dd9db6201dc4a27048e0796384b24e17caf74b
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2021-10-27 11:17:51 +0200

    Remove access tokens from source files

--- src/main/frontend/api_request.js
+++ src/main/frontend/api_request.js
@@ -6,35 +6,35 @@ function get_time_table(eva, date, time) {
   fetch(url, {
     headers: {
       Accept: "application/xml",
-      Authorization: "Bearer fc4f82d6be092177015ebbb25b56563a" // API Key
+      Authorization: "XXX"      // TODO: API Key
     }
   })
   .then(function(resp) {        // handles the API GET-response
         return resp.text();     // get the Body of the Response (omit meta data)
-  }) 
+  })
   .then(function(data){
       let parser = new DOMParser();
       xmlDoc = parser.parseFromString(data, 'text/xml');
-  
+
       let stops = xmlDoc.getElementsByTagName('s');         // s = stop(Haltestelle)
-        
-      let stopsList = [];    
+
+      let stopsList = [];
 
       for (let i = 0; i < stops.length; i++) {
         let stop = [],
-        category = stops[i].getElementsByTagName('tl')[0].getAttribute('c'),   // ICE, RE, S, Bus,... 
-        departure = stops[i].getElementsByTagName('dp')[0];             
+        category = stops[i].getElementsByTagName('tl')[0].getAttribute('c'),   // ICE, RE, S, Bus,...
+        departure = stops[i].getElementsByTagName('dp')[0];
 
         if (departure != undefined) {                                          // only consider departure, no arrivals
           let depTime = departure.getAttribute('pt'),                          // YYMMDDHHmm
-          platform = departure.getAttribute('pp'),                             
+          platform = departure.getAttribute('pp'),
           plannedPath = departure.getAttribute('ppth');
 
           stop.push(category, depTime, platform, plannedPath)
           stopsList.push(stop)                                                 // append stop[i] information (strings) to stoplist
-        }     
+        }
       }
-      
+
       console.log(stopsList)
   });
 }
