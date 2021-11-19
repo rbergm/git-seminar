10a70c21f72ab6f37aa0c9a4e8240bd9446bac4b
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2021-11-19 11:47:13 +0100

    Test routing on full trainline graph

--- src/test/java/de/tudresden/geo/gitseminar/routing/TargetRoutingServiceTests.java
+++ src/test/java/de/tudresden/geo/gitseminar/routing/TargetRoutingServiceTests.java
@@ -1,11 +1,19 @@
 package de.tudresden.geo.gitseminar.routing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
 import org.jgrapht.Graph;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import de.tudresden.geo.gitseminar.routing.network.TrainLineNetworkParser;
 import de.tudresden.geo.gitseminar.util.Count;
 
 public class TargetRoutingServiceTests {
+
+	private static final Logger log = LoggerFactory.getLogger(TargetRoutingServiceTests.class);
 
 	private TargetRoutingService routingService = new TargetRoutingService();
 	private Graph<TrainStation, StationConnection> network = ExampleRoutingData.get();
@@ -43,8 +51,28 @@ public class TargetRoutingServiceTests {
 		assertThat(result).isEmpty();
 	}
 
+	@Test
+	public void findsClosestNodeOnLargeGraphs() throws IOException {
+		Resource networkDescription = new ClassPathResource("timetables.json");
+		var networkParser = new TrainLineNetworkParser();
 
-	// TODO more extensive testing on larger datasets
+		var network = networkParser.loadFromJsonFile(networkDescription.getFile());
+		assertThat(network).isPresent();
+
+
+		var start = TrainStation.ofName("Großheringen").get();
+		var goalSpec = new OberzentrumTargetSpecification();
+
+		var result = routingService.findTarget(network.get(), start, goalSpec);
+		log.debug("First route: {}", result);
+		assertThat(result).isPresent();
+
+		start = TrainStation.ofName("Kurort Oberwiesenthal").get();
+		result = routingService.findTarget(network.get(), start, goalSpec);
+		log.debug("Second route: {}", result);
+		assertThat(result).isPresent();
+	}
+
 
 	private static class ImpossibleTargetSpecification implements TargetSpecification {
 
