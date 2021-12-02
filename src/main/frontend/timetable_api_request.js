f9db6de0f12ecf12cc693399359147915dca8f29
jutoth <92805267+jutoth@users.noreply.github.com>
2021-12-02 17:41:10 +0100

    Add files via upload

--- src/main/frontend/timetable_api_request.js
+++ src/main/frontend/timetable_api_request.js
@@ -13,10 +13,8 @@ async function get_time_table(eva, name, plus_x_hours) {
   Month = Month.toString();
   let Year = DateTime.getFullYear().toString().substring(2); // 2 digits representation of year
   let time = (DateTime.getHours() < 10 ? '0' : '') + (DateTime.getHours().toString()); // add leading 0 to numbers < 10
-
   let date = Year+Month+Day; // concatanate to YYMMDD
-
-  let url = "https://api.deutschebahn.com/timetables/v1/plan/" + eva +"/" + date +"/" + time;
+  let url  = "https://api.deutschebahn.com/timetables/v1/plan/" + eva +"/" + date +"/" + time;
 
 await fetch(url, {
     headers: {
@@ -30,19 +28,18 @@ await fetch(url, {
   .then(function(data){
       let parser = new DOMParser();
       let xmlDoc = parser.parseFromString(data, 'text/xml');
-  
-      let stops = xmlDoc.getElementsByTagName('s');         // s = stop(Haltestelle)
+      let stops  = xmlDoc.getElementsByTagName('s');         // s = stop(Haltestelle)
     
       let stopsList = [];    
 
       for (let i = 0; i < stops.length; i++) {
-        let stop = [],
-        category = stops[i].getElementsByTagName('tl')[0].getAttribute('c'),   // ICE, RE, S, Bus,... 
+        let stop  = [],
+        category  = stops[i].getElementsByTagName('tl')[0].getAttribute('c'),   // ICE, RE, S, Bus,... 
         departure = stops[i].getElementsByTagName('dp')[0];             
 
         if (departure != undefined) {                                          // only consider departure, no arrivals
           let depTime = departure.getAttribute('pt'),                          // YYMMDDHHmm
-          platform = departure.getAttribute('pp'),                             
+          platform    = departure.getAttribute('pp'),                             
           plannedPath = departure.getAttribute('ppth');
 
           stop.push(category, depTime, platform, plannedPath)
@@ -74,26 +71,38 @@ await fetch(url, {
     for (let index = 0; index < timetables.length; index++) {
 
 
-
+      
       let traintype = timetables[index][0];
-      let datetime = timetables[index][1];
-      let platform = timetables[index][2];
-      let route = timetables[index][3];
-
+      let datetime  = timetables[index][1];
+      let platform  = timetables[index][2];
+      let route     = timetables[index][3]
+      console.log(route);
+      
       let formatted_date = datetime.substring(4,6) + '.' + datetime.substring(2,4) + '.' + datetime.substring(0,2);
       let formatted_time = datetime.substring(6,8) + ':' + datetime.substring(8);
 
-      let new_text= traintype + " auf Gleis " + platform + ' am ' + formatted_date + " um " + formatted_time + " Richtung " + (route.substring(route.lastIndexOf("|")+1));
+      let tr_text= traintype + " auf Gleis " + platform + ' am ' + formatted_date + " um " + formatted_time + " Richtung " + (route.substring(route.lastIndexOf("|")+1));
 
       let row = table.insertRow();
-      let cell1 = row.insertCell();
-  
-      let newText = document.createTextNode(new_text);
-
-      cell1.appendChild(newText);
+      row.id = 'row_departure_table';
+      let hidden_row = table.insertRow();
+      hidden_row.style.display = 'none'
       
-      
+      let cell         = row.insertCell();
+      let hidden_cell  = hidden_row.insertCell();
+      let tr_textnode  = document.createTextNode(tr_text);
+      let hidden_route = document.createTextNode(route);
 
+      row.onclick = function() {
+        if (hidden_row.style.display == 'none') {
+          hidden_row.style.display = 'inline';
+        }else if (hidden_row.style.display = 'inline'){
+          hidden_row.style.display = 'none';
+        }
+      };
+    
+      cell.appendChild(tr_textnode);
+      hidden_cell.appendChild(hidden_route);
 
     }
   
