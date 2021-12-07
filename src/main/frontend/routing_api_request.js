40b73637b8d2aecc41594cfff063406a3d3e3866
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2021-12-07 17:22:28 +0100

    Add client code to fetch processing service route

--- src/main/frontend/routing_api_request.js
+++ src/main/frontend/routing_api_request.js
@@ -20,43 +20,102 @@ async function getRoute(marker_lng, marker_lat, station_lng, station_lat, profil
     };
 
 
-    while(instructions.hasChildNodes()){
+    while (instructions.hasChildNodes()) {
         instructions.removeChild(instructions.firstChild);
     }
     for (const step of steps) {
 
-            let li = document.createElement('li');
-            li.className = 'list-group-item';
-            li.appendChild(document.createTextNode(step.maneuver.instruction));
-            instructions.appendChild(li);             
-    }   
+        let li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode(step.maneuver.instruction));
+        instructions.appendChild(li);
+    }
 
-        // if the route already exists on the map, we'll reset it using setData
+    // if the route already exists on the map, we'll reset it using setData
     if (map.getSource('route')) {
         map.getSource('route').setData(geojson);
     }
     // otherwise, we'll make a new request
     else {
         map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-            type: 'geojson',
-            data: geojson
-        },
-        layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        paint: {
-            'line-color': '#3887be',
-            'line-width': 5,
-            'line-opacity': 0.75
-        }
-    });
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: geojson
+            },
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            paint: {
+                'line-color': '#3887be',
+                'line-width': 5,
+                'line-opacity': 0.75
+            }
+        });
 
-    
+
+    }
+
+
 }
 
+async function getRouteFromProcessingService(stationName) {
+    const init = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            startStation: stationName,
+            target: "oberzentrum" // "mittelzentrum"
+        })
+    }
 
+    const req = new Request("http://localhost:8000/routing/calculate");
+
+    const route = await fetch(req, init).then((response) => {
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+    });
+
+    return route;
+}
+
+// structure of the processing service response
+const exampleRoute = {
+    status: "ok",
+    route: {
+        // normal route info
+        startStation: {
+            name: "Freiberg(Sachs)",
+            eva: "",
+            mittelzentrum: true,
+            oberzentrum: false
+        },
+        finalStop: {
+            name: "Chemnitz(Hbf)",
+            eva: "",
+            mittelzentrum: false,
+            oberzentrum: true
+        },
+        changes: [
+            {
+                station: {
+                    name: "Freiberg(Sachs)",
+                    eva: "",
+                    mittelzentrum: true,
+                    oberzentrum: false
+                },
+                targetLine: {
+                    name: "3"
+                },
+                trainEntry: true
+            }
+        ],
+        effectiveChanges: []
+    }
 }
