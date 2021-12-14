658112200b6c76517c4f283483954df4936b13c6
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2021-12-14 17:48:45 +0100

    Extend raster calculation client example

--- src/main/resources/static/js/raster_calculation_tests.js
+++ src/main/resources/static/js/raster_calculation_tests.js
@@ -3,7 +3,7 @@ let reqData = {
     mittelzentrumDistanceWeight: 7,
     oberzentrumDistanceWeight: 9,
     stationDistanceWeight: 3,
-    populationDataWeight: 1
+    populationDataWeight: 8
 };
 
 let init = {
@@ -12,14 +12,18 @@ let init = {
     body: JSON.stringify(reqData)
 };
 
-
-
 let req = new Request("http://localhost:8000/raster/calculate");
 
-
-const route = await fetch(req, init).then((response) => {
+const layer = await fetch(req, init).then((response) => {
     if (!response.ok) {
         throw new Error(`Error: ${response.status} - ${response.statusText}`);
     }
     return response.json();
 });
+
+// structure of response
+
+let response = {
+    status: "created", // "created" or "cached"
+    geoserverUrl: "raster_depf2.0_mzdist7.0_ozdist9.0_statdist3.0_pop1.0" // ID of the WMS/WCS layer
+}
